42a316c4babe17d8c48ee8b1e9e4cf2536df1544
juyoung <dilatata96@gmail.com>
2021-11-02 18:53:42 +0900

    user html 생성, 수정, 연결

--- DjangoBackend/user/views.py
+++ DjangoBackend/user/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from django.db import models
+from function.models import PredData
 # Create your views here.
 def user(request):
-    return render(request,'user/user.html')
+    context = {''}
+    return render(request,'user/login.html', context)
+
+class PostListView(ListView):
+    template_name = "user/mypage.html"  #목록페이지 (응답페이지)
+    model = PredData #데이터를 조회할 Model클래스
+    paginate_by = 10 # 한번(한페이지)에 10개씩만 조회
+
