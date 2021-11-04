3f9f7ae25972d5f3f92de5aa3ccfc1643e8aa0fa
juyoung <dilatata96@gmail.com>
2021-11-04 14:33:09 +0900

    list, detail 정보 전달 기능, 화면 연결

--- DjangoBackend/user/views.py
+++ DjangoBackend/user/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.db import models
 from function.models import PredData
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.urls import reverse_lazy
+
 # Create your views here.
 def user(request):
     context = {''}
     return render(request,'user/login.html', context)
 
+def board_detail(request, pk):
+    board = PredData.objects.get(pk=pk)
+    # pk 에 해당하는 글을 가지고 올 수 있게 된다.
+    return render(request, 'user/board_detail.html', {'board':board})  
+
+@method_decorator(login_required, name="dispatch")
 class PostListView(ListView):
     template_name = "user/mypage.html"  #목록페이지 (응답페이지)
     model = PredData #데이터를 조회할 Model클래스
-    # paginate_by = 10 # 한번(한페이지)에 10개씩만 조회
-    # 통 DB 는 object_list 로 넘어간다.
+    # django는 자동으로 연결한 DB의 정보가 object_list라는 이름으로 넘어감.
+
+    def get_success_url(self):
+        return reverse_lazy('board:detail', args=[self.object.pk])
+    
+    
